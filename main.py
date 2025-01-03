ade8ed2652d92351dabb2c27849951310b0ce45e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2025-01-03 19:32:26 +0530

    detect object using images

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ model = YOLO("models/best.pt")
 # model.predict(source="video/video.mp4", save=True, show=True)           
 
 #detect object using images
-# model.predict(source="img/vander-films-v8CuPrOvKUA-unsplash.jpg", save=True, show=True)         
-# model.predict(source="img/img.jpg", save=True, show=True)         
+model.predict(source="img/vander-films-v8CuPrOvKUA-unsplash.jpg", save=True, show=True)         
+model.predict(source="img/img.jpg", save=True, show=True)         
 
 # detect object using url  
 # model.predict(source="https://media.istockphoto.com/id/481220364/photo/beautiful-couple-cycling-on-the-road.jpg?s=2048x2048&w=is&k=20&c=0fBHn2PRrYhNNZcjWV7blBnEDmFDCiisuPsdHoU8bZU=", save=True, show=True)           
 
 #detect object using webcam
-model.predict(source="0", show=True)     
+# model.predict(source="0", show=True)     
 
   
